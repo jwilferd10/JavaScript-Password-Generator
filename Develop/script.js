f6f6b7211c0e17246c4426c54480e76fb197e84b
jwilferd <jwilferd10@yahoo.com>
2022-01-18 18:36:05 -0800

    Feat: Saved file, ensuring code is properly committed

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // passwordOptions contains all necessary string data needed to generate the password
 const passwordOptions = {
   num: "1234567890",
@@ -13,6 +12,9 @@ let generatePassword = function() {
   // initial state for password information
   let passInfo = "";
 
+  // list of chosen characters
+  const passChars = [];
+
   // ask user for the length of their password
   let characterAmount = window.prompt("Enter the amount of characters you want for your password. NOTE: Must be between 8-128 characters");
 
@@ -20,39 +22,47 @@ let generatePassword = function() {
   if (characterAmount >= 8 && characterAmount < 129) {
 
     // ask if user wants to include integers
-    let getInteger = window.confirm("Would you like to include NUMBERS?");
+    const getInteger = window.confirm("Would you like to include NUMBERS?");
 
     // if user wants to include numbers
     if (getInteger) {
       // add numerical characters to password data 
-      passInfo = passInfo + passwordOptions.num;
+      passInfo += passwordOptions.num;
+      // add a number to the array of chosen characters
+      passChars.push(getRandomChar(passwordOptions.num));
     };
 
     // ask if user wants to include special characters
-    let getSpecialCharacters = window.confirm("Would you like to include SPECIAL characters?");
+    const getSpecialCharacters = window.confirm("Would you like to include SPECIAL characters?");
 
     // if user wants to include special characters 
     if (getSpecialCharacters) {
       // add special characters to password data
-      passInfo = passInfo + passwordOptions.specialChar;
+      passInfo += passwordOptions.specialChar;
+      // add a special character to the array of chosen characters
+      passChars.push(getRandomChar(passwordOptions.specialChar));
     };
 
     // ask if user wants to include lowercase characters
-    let getLowerCase = window.confirm("Would you like to include LOWERCASE characters?");
+    const getLowerCase = window.confirm("Would you like to include LOWERCASE characters?");
 
     // if user wants to include lowercase characters
     if (getLowerCase) {
       // add lowercase characters to password data
-      passInfo = passInfo + passwordOptions.lowerCase;
+      passInfo += passwordOptions.lowerCase;
+      // add a lowercase character to the list of chosen characters
+      passChars.push(getRandomChar(passwordOptions.lowerCase));
     };
 
     // ask if user wants to include uppercase characters
-    let getUpperCase = window.confirm("Would you like to include UPPERCASE characters?");
+    const getUpperCase = window.confirm("Would you like to include UPPERCASE characters?");
 
     // if user wants to include uppercase characters
     if (getUpperCase) {
       // add uppercase characters to password data 
-      passInfo = passInfo + passwordOptions.upperCase;
+      passInfo += passwordOptions.upperCase;
+      // add a uppercase character to the list of chosen characters
+      passChars.push(getRandomChar(passwordOptions.upperCase));
     };
 
     // ensure user chooses at least one option
@@ -84,6 +94,11 @@ let generatePassword = function() {
   }
 };
 
+// getRandomChar pulls from the passChars array, fromString processes the value
+let getRandomChar = function(fromString) {
+  return fromString[Math.floor(Math.random() * fromString.length)];
+}
+
 // References to the #generate element
 let generateBtn = document.querySelector("#generate");
 
@@ -99,128 +114,4 @@ function writePassword() {
 }
 
 // The event listener looks for a click to then look at the function writePassword
-=======
-//First: Execute a click event that fires up the prompts NOTE: REMEMBER TO INTERACT WITH PROMPT EVERY CHANGE
-//Second: User MUST choose a length of 8 - 128 characters for the password
-//Third: Create a series of  if prompts that execute after one another, if invalid option given return the user
-//Fourth: Find a method to accept user inputs for finale
-//Fifth: Create a function, likely math.(something) to randomize and generate password
-
-//REFERENCE THESE OPTIONS FURTHER DOWN THE CODE
-// ABCDEFGHIJKLMNOPQRSTUVWXYZ
-// abcdefghijklmnopqrstuvwxyz
-// 1234567890
-// !@#$%&'()*+,^-./:;<=>?[]_`{~}|
-
-//GLOBAL VARIABLES
-
-// The global variables go in this order -> lowercase letters, capital letters, numbers, special characters, userChoices, randomPassword
-var lowerAns = false;
-var capitalAns = false;
-var numbersAns = false;
-var specialAns = false;
-var userChoices = "";
-var randomPassword;
-
-// These global variables will cover our characters/numbers
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers ="1234567890";
-var specialChar = "!@#$%&'()*+,^-./:;<=>?[]_`{~}|";
-
-
-// Executes when the red button is clicked, finally realized to plug in generatePassword
-function generatePassword() {
-
-  // set variables to start values 
-  var lowerAns = false;
-  var capitalAns = false;
-  var numbersAns = false;
-  var specialAns = false;
-  var userChoices = "";
-
-  // Asking user for the length of the password
-  var characterlength = window.prompt("Enter amount of characters for password, MUST BE 8 and NO MORE than 128 characters!");
-
-  // If the character length doesn't match requirements, alert the user
-  if (characterlength >= 8 && characterlength < 129) {
-    specialAns = window.confirm("Do you want to add special characters");
-  }
-  
-  // If the character length doesn't match requirements, alert the user 
-  else {
-    window.alert("You need to provide a valid length! Please try again");
-    return
-  }
-  
-  // If user wants special characters, add them to userChoices
-  if (specialAns) {
-    specialAns = true;
-    userChoices = userChoices + specialChar;
-  }
-
-  //Ask user if they want to use lowercase letters
-  lowerAns = window.confirm("Do you need lowercase letters?");
-
-  // if user confirms lowercase letters, add to userChoices
-  if (lowerAns) {
-    lowerAns = true;
-    userChoices = userChoices + lower;
-  }
-
-  //Ask user if they want to use capital letters
-  capitalAns = window.confirm("Do you need uppercase letters?");
-
-  // if user confirms capital letters, add to userChoices
-  if (capitalAns) {
-    capitalAns = true;
-    userChoices = userChoices + capital;
-  }
-
-  //Ask user if they want to use numbers
-  numbersAns = window.confirm("Do you want to add numbers?");
-
-  // if user confirms numbers, add to userChoices
-  if (numbersAns) {
-    numbersAns = true;
-    userChoices = userChoices + numbers;
-  }
-  // If the user does not pick a password selector request they use at least one valid selector and return them to selector questions
-  if (specialAns !=true && numbersAns !=true && capitalAns !=true && lowerAns !=true) {
-    window.alert("You must select at least one option! Please try again.");
-    generatePassword();
-  }
-
-  // This sets randomPassword to an empty string
-  var randomPassword = ""
-
-  // for loop to select a list of characters that are randomly chosen from the userChoices character preferences AND as long characterlength
-  
-  // Remember: i starts at 0, i specified tries to match length, i++ adds
-  for (var i = 0; i < characterlength; i++) {
-     var random = Math.floor(Math.random() * userChoices.length);
-
-     randomPassword = randomPassword + userChoices.charAt(random);
-   }
-
-   // Display random password
-   return randomPassword;
-};   
-  
-// Get references to the #generate element (We'll likely be ignoring this)
-var generateBtn = document.querySelector("#generate");
-
-//Write password to the #password input
-function writePassword() {
- 
-  //we'll likely use this.
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
->>>>>>> af7b332907a7755d3194d9982eab7151faa4daf5
 generateBtn.addEventListener("click", writePassword);
